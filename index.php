3ec37b32d209856df59128fc8c750b09819b6b57
mrspider94 <48424837+mrspider94@users.noreply.github.com>
2019-03-11 10:33:56 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
                 </div>
                 <div class="p-item">
                     <i class="far fa-clock"></i>
-                    <p>Enter an email where to send results of the crawl</p>
+                    <p>Enter an email where to send results of the crawl every 24 hours</p>
                     <input type="text" name="inp_email" placeholder="Enter your email">
                 </div>
                 <div class="p-item">
