3a3ea62551a6c153c51f5b0e1070b3a780e11d6a
mrspider94 <48424837+mrspider94@users.noreply.github.com>
2019-03-11 10:43:23 +0200

    Add files via upload

--- includes/sql.php
+++ includes/sql.php
@@ -1,11 +1,9 @@
 <?php 
 // connect to database
-$conn = mysqli_connect("localhost", "u928739372_abam", "KAZKOKSAI", "u928739372_abam");
+$conn = mysqli_connect("localhost", "USERNAME", "PASSWORD", "DATABASE");
 if (!$conn) {
     die("Error connecting to database: " . mysqli_connect_error());
 }
 // define global constants
 define ('ROOT_PATH', realpath(dirname(__FILE__)));
-define('BASE_URL', 'https://mrspider.000webhostapp.com/');
-date_default_timezone_set('Europe/Vilnius');
 ?>
